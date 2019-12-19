c5878d4e0720bbbcf6f3fd377c8da457af775d5d
El'dar Nasyrov <buugaaga@bk.ru>
2019-12-19 19:43:23 +0500

    i added components Display and ButtonPanel. added handler to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import Display from './componets/Display';
+import ButtonPanel from './componets/ButtonPanel';
+import './App.css';
+
 const App: React.FC = () => {
+
+  function handleClick(event) {
+    console.log(event.target);
+  }
+
   return (
     <div className="App">
       <h1>hi babay</h1>
+      <Display />
+      <ButtonPanel handleClick={handleClick} />
     </div>
   );
 }
