7e24c00bd5c47532459a89ddd46015b5a889f1d0
El'dar Nasyrov <buugaaga@bk.ru>
2019-12-19 14:21:14 +0300

    Updates

---
+++ precache-manifest.1be8b0249fd0d64ff338a199aa6057c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2beb3aa8d6b946b656397c4b3e7d4fe6",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "06446502f8ab89c52924",
+    "url": "/calculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "75bb5127fb0779c4a924",
+    "url": "/calculator/static/js/2.f7a61c8c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.f7a61c8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06446502f8ab89c52924",
+    "url": "/calculator/static/js/main.761e0472.chunk.js"
+  },
+  {
+    "revision": "c3d9a3bbcb6bc1177d26",
+    "url": "/calculator/static/js/runtime-main.250539d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
